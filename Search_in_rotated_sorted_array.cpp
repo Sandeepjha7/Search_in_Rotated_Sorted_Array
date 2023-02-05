98cb5e3a961a71512e4514fe870acda87264afbd
Sandeep Jha <112749569+Sandeepjha7@users.noreply.github.com>
2023-02-06 00:12:39 +0530

    Add files via upload

---
+++ Search_in_rotated_sorted_array.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std; 
+
+int getPivot(int arr[],int n)
+{
+    int s=0;
+    int e=n-1;
+    int mid = s+(e-s)/2 ;
+
+    while (s<e)
+    {
+        if(arr[mid]>=arr[0])
+        {
+            s=mid+1;
+        }
+        else
+        {
+            e=mid;
+        }
+        mid = s+(e-s)/2 ;
+    }
+    return s;
+}
+int binarySearch(int arr[],int start,int end,int key )
+{
+    
+    int mid = start+(end-start)/2 ;
+    
+    while(start<=end)
+    {
+        if(arr[mid]==key)
+          return mid;
+        else if(key>arr[mid])
+        {
+            start=mid+1;
+        }
+        else if(key<arr[mid])
+        {
+            end=mid-1;
+        }
+        mid = start+(end-start)/2 ;
+    }
+    return -1;
+}
+int findPosition(int arr[], int n, int k)
+{
+    int pivot = getPivot(arr,n);
+
+    if(k>=arr[pivot] && k<=arr[n-1])
+
+    {//binary search on second line
+        return binarySearch(arr,pivot,n-1,k);
+    }
+    else 
+    
+    {//binary search on first line
+        return binarySearch(arr,0,pivot-1,k);
+    }
+}
+int main()
+{
+    int arr[5]={3,8,10,1,17};
+    
+    cout<<"Search in rotated sorted array is "<<findPosition(arr,5,10)<<endl;
+
+    return 0;
+}
+
